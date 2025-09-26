a77b88f7be5ca0662c39a9a876ddd78e21ad13d7
jimen <jimen@DESKTOP-PKDPQNU>
2025-09-26 11:16:00 +0200

    Actualizar .gitignore y ReservaRuralApp.java

--- src/reservaruralapp/ReservaRuralApp.java
+++ src/reservaruralapp/ReservaRuralApp.java
@@ -1,20 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package reservaruralapp;
 
-/**
- *
- * @author jimen
- */
+
 public class ReservaRuralApp {
 
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        System.out.println("Este es el primer paso de la nueva aplicacion, probando github");
     }
     
 }
