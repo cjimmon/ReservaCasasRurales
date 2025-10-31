0c3935bb3ac5eee7780dc9c6f3b1d805529861c8
jimen <jimen@DESKTOP-PKDPQNU>
2025-10-31 14:09:32 +0100

    "tabla nueva reserva esquema"

--- src/reservaruralapp/Principal.java
+++ src/reservaruralapp/Principal.java
@@ -57,6 +57,25 @@ public class Principal extends javax.swing.JFrame {
         Bienvenida = new javax.swing.JLabel();
         PanelNuevaReservas = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        BoxNombreNuevaReserva = new javax.swing.JTextField();
+        BoxApellidosNuevaReserva = new javax.swing.JTextField();
+        BoxDniNuevaReserva = new javax.swing.JTextField();
+        BoxTelefonoNuevaReserva = new javax.swing.JTextField();
+        BoxEmailNuevaReserva = new javax.swing.JTextField();
+        BoxCasaNuevaReserva = new javax.swing.JComboBox<>();
+        jLabel13 = new javax.swing.JLabel();
+        jLabel14 = new javax.swing.JLabel();
+        jLabel15 = new javax.swing.JLabel();
+        CalendarioInicioNuevaReserva = new com.toedter.calendar.JCalendar();
+        CalendarioFinNuevaReserva = new com.toedter.calendar.JCalendar();
+        jLabel16 = new javax.swing.JLabel();
+        SpinnerNumeropersonaNuevaReserva = new javax.swing.JSpinner();
+        BotonGuardarNuevaReserva = new javax.swing.JButton();
         PanelClientes = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
         PanelCasas = new javax.swing.JPanel();
@@ -187,23 +206,163 @@ public class Principal extends javax.swing.JFrame {
 
         jPanel3.add(PanelBienvenida, "card7");
 
-        jLabel1.setText("nueva reserva");
+        jLabel1.setText("NUEVA RESERVA");
+
+        jLabel8.setText("Nombre:");
+
+        jLabel9.setText("Apellidos:");
+
+        jLabel10.setText("DNI:");
+
+        jLabel11.setText("Telefono:");
+
+        jLabel12.setText("Email:");
+
+        BoxNombreNuevaReserva.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BoxNombreNuevaReservaActionPerformed(evt);
+            }
+        });
+
+        BoxApellidosNuevaReserva.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BoxApellidosNuevaReservaActionPerformed(evt);
+            }
+        });
+
+        BoxDniNuevaReserva.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BoxDniNuevaReservaActionPerformed(evt);
+            }
+        });
+
+        BoxTelefonoNuevaReserva.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BoxTelefonoNuevaReservaActionPerformed(evt);
+            }
+        });
+
+        BoxEmailNuevaReserva.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BoxEmailNuevaReservaActionPerformed(evt);
+            }
+        });
+
+        BoxCasaNuevaReserva.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Casa 1", "Casa 2" }));
+        BoxCasaNuevaReserva.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BoxCasaNuevaReservaActionPerformed(evt);
+            }
+        });
+
+        jLabel13.setText("Casa:");
+
+        jLabel14.setText("Fecha Inicio:");
+
+        jLabel15.setText("Fecha Fin:");
+
+        jLabel16.setText("Nº Personas:");
+
+        BotonGuardarNuevaReserva.setText("Guardar Reserva");
+        BotonGuardarNuevaReserva.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonGuardarNuevaReservaActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout PanelNuevaReservasLayout = new javax.swing.GroupLayout(PanelNuevaReservas);
         PanelNuevaReservas.setLayout(PanelNuevaReservasLayout);
         PanelNuevaReservasLayout.setHorizontalGroup(
             PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(PanelNuevaReservasLayout.createSequentialGroup()
-                .addGap(36, 36, 36)
-                .addComponent(jLabel1)
-                .addContainerGap(737, Short.MAX_VALUE))
+                .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(PanelNuevaReservasLayout.createSequentialGroup()
+                        .addGap(36, 36, 36)
+                        .addComponent(jLabel1))
+                    .addGroup(PanelNuevaReservasLayout.createSequentialGroup()
+                        .addGap(39, 39, 39)
+                        .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(jLabel9)
+                                .addComponent(jLabel8, javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(PanelNuevaReservasLayout.createSequentialGroup()
+                                    .addComponent(jLabel10)
+                                    .addGap(27, 27, 27)))
+                            .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jLabel12)
+                                .addComponent(jLabel11)))
+                        .addGap(18, 18, 18)
+                        .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(BoxTelefonoNuevaReserva, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 123, Short.MAX_VALUE)
+                            .addComponent(BoxDniNuevaReserva, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(BoxApellidosNuevaReserva, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(BoxNombreNuevaReserva, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(BoxEmailNuevaReserva))
+                        .addGap(80, 80, 80)
+                        .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(PanelNuevaReservasLayout.createSequentialGroup()
+                                .addComponent(jLabel13)
+                                .addGap(18, 18, 18)
+                                .addComponent(BoxCasaNuevaReserva, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(64, 64, 64)
+                                .addComponent(jLabel16)
+                                .addGap(18, 18, 18)
+                                .addComponent(SpinnerNumeropersonaNuevaReserva, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(PanelNuevaReservasLayout.createSequentialGroup()
+                                .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel14)
+                                    .addComponent(jLabel15))
+                                .addGap(28, 28, 28)
+                                .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(CalendarioFinNuevaReserva, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(CalendarioInicioNuevaReserva, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(BotonGuardarNuevaReserva))))
+                .addContainerGap(202, Short.MAX_VALUE))
         );
         PanelNuevaReservasLayout.setVerticalGroup(
             PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(PanelNuevaReservasLayout.createSequentialGroup()
                 .addGap(23, 23, 23)
                 .addComponent(jLabel1)
-                .addContainerGap(511, Short.MAX_VALUE))
+                .addGap(58, 58, 58)
+                .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel8)
+                    .addComponent(BoxNombreNuevaReserva, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BoxCasaNuevaReserva, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel13)
+                    .addComponent(jLabel16)
+                    .addComponent(SpinnerNumeropersonaNuevaReserva, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(22, 22, 22)
+                .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(PanelNuevaReservasLayout.createSequentialGroup()
+                        .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel9)
+                            .addComponent(BoxApellidosNuevaReserva, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel14))
+                        .addGap(25, 25, 25)
+                        .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel10)
+                            .addComponent(BoxDniNuevaReserva, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(27, 27, 27)
+                        .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel11)
+                            .addComponent(BoxTelefonoNuevaReserva, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(PanelNuevaReservasLayout.createSequentialGroup()
+                                .addGap(21, 21, 21)
+                                .addGroup(PanelNuevaReservasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                    .addComponent(jLabel12)
+                                    .addComponent(BoxEmailNuevaReserva, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(PanelNuevaReservasLayout.createSequentialGroup()
+                                .addGap(38, 38, 38)
+                                .addComponent(jLabel15))))
+                    .addGroup(PanelNuevaReservasLayout.createSequentialGroup()
+                        .addComponent(CalendarioInicioNuevaReserva, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(CalendarioFinNuevaReserva, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
+                .addComponent(BotonGuardarNuevaReserva)
+                .addGap(57, 57, 57))
         );
 
         jPanel3.add(PanelNuevaReservas, "card2");
@@ -464,6 +623,34 @@ public class Principal extends javax.swing.JFrame {
 
     }//GEN-LAST:event_botonGuardarCambiosActionPerformed
 
+    private void BoxDniNuevaReservaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BoxDniNuevaReservaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BoxDniNuevaReservaActionPerformed
+
+    private void BoxApellidosNuevaReservaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BoxApellidosNuevaReservaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BoxApellidosNuevaReservaActionPerformed
+
+    private void BoxNombreNuevaReservaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BoxNombreNuevaReservaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BoxNombreNuevaReservaActionPerformed
+
+    private void BoxTelefonoNuevaReservaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BoxTelefonoNuevaReservaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BoxTelefonoNuevaReservaActionPerformed
+
+    private void BoxEmailNuevaReservaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BoxEmailNuevaReservaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BoxEmailNuevaReservaActionPerformed
+
+    private void BoxCasaNuevaReservaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BoxCasaNuevaReservaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BoxCasaNuevaReservaActionPerformed
+
+    private void BotonGuardarNuevaReservaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonGuardarNuevaReservaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BotonGuardarNuevaReservaActionPerformed
+
 
     
     
@@ -472,9 +659,18 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JLabel Bienvenida;
     private javax.swing.JButton BotonCasas;
     private javax.swing.JButton BotonClientes;
+    private javax.swing.JButton BotonGuardarNuevaReserva;
     private javax.swing.JButton BotonInformes;
     private javax.swing.JButton BotonNuevaReserva;
     private javax.swing.JButton BotonOpciones;
+    private javax.swing.JTextField BoxApellidosNuevaReserva;
+    private javax.swing.JComboBox<String> BoxCasaNuevaReserva;
+    private javax.swing.JTextField BoxDniNuevaReserva;
+    private javax.swing.JTextField BoxEmailNuevaReserva;
+    private javax.swing.JTextField BoxNombreNuevaReserva;
+    private javax.swing.JTextField BoxTelefonoNuevaReserva;
+    private com.toedter.calendar.JCalendar CalendarioFinNuevaReserva;
+    private com.toedter.calendar.JCalendar CalendarioInicioNuevaReserva;
     private javax.swing.JPanel PanelBienvenida;
     private javax.swing.JPanel PanelBuscarReserva;
     private javax.swing.JPanel PanelCasas;
@@ -482,6 +678,7 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JPanel PanelInformes;
     private javax.swing.JPanel PanelNuevaReservas;
     private javax.swing.JPanel PanelOpciones;
+    private javax.swing.JSpinner SpinnerNumeropersonaNuevaReserva;
     private javax.swing.JButton botonBuscarReserva;
     private javax.swing.JButton botonEjecutarBusqueda;
     private javax.swing.JButton botonGuardarCambios;
@@ -489,12 +686,21 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> boxBuscarReserva;
     private javax.swing.JTextField campoBusqueda;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
+    private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JLabel logo2;
